4450626b6ac210c140d2955bbf304266d6797ac5
audrey-sun-99 <audrey.sun@stu.ocsb.ca>
2026-02-11 12:46:10 -0500

    i changed the name

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Audrey
+ * Created on: feb 20206
+ * This program should show my name
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello,Audrey!')
